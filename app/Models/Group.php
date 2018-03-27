6e88da7fa00799b97ea2636f18bb17f9c858a987
Marko <marko.laviner>
2018-03-27 10:09:02 +0200

    softdelete added to group

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Group extends Model
 {
-    //
-
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -15,6 +15,6 @@ class Group extends Model
      * @var array
      */
     protected $fillable = [
-        'group_name', 'description'
+        'group_name', 'description', 'meta', 'description'
     ]; 
 }
