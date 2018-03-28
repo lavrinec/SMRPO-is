30bb559aa52503483750e6ec6de20408a736fdda
Žiga Černigoj <zc6857@student.uni-lj.si>
2018-03-28 19:21:18 +0200

    added view for list of boards

---
+++ resources/views/boards/create.blade.php
@@ -0,0 +1,86 @@
+@extends('default.layout')
+
+@section('content')
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Tabla
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li><a href="#">Examples</a></li>
+                <li class="active">Board</li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="row">
+                <div class="col-md-12">
+
+                    <div class="box">
+                        <div class="box-header">
+                            <h3 class="box-title">Ustvarjanje nove table</h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+
+                            {{--{{ action('UserController@store') }}--}}
+
+                            <form class="form-horizontal" method="POST" action="{{ action('BoardController@store') }}">
+
+                                @csrf
+
+                                <div class="form-group">
+                                    <label for="board_name" class="col-sm-2 control-label">Ime table</label>
+
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="board_name" name="board_name"
+                                               placeholder="Ime" pattern=".{1,255}" required
+                                               title="Ime naj bo dolgo med 8 in 255 znakov">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="description" class="col-sm-2 control-label">Opis</label>
+
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="description" name="description"
+                                               placeholder="Opis" pattern=".{1,255}" required
+                                               title="Priimek naj bo dolg med 1 in 255 znakov">
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <div class="col-sm-offset-2 col-sm-4">
+                                        <button type="submit" class="btn btn-primary">Ustvari tablo</button>
+                                    </div>
+                                    <div class="col-sm-offset-4 col-sm-2">
+                                        <a href="{{ action('BoardController@index') }}" class="btn btn-danger btn-block"><b>Prekliči</b></a>
+                                    </div>
+                                </div>
+                            </form>
+
+
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+
+
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
