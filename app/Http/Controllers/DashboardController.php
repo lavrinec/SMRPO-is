bb3d2e4fdd601fc73e7a85415dd3659f5e5558b1
Marko <marko.laviner>
2018-03-29 13:31:38 +0200

    dashboard update

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('content.maincontent');
+    }
+}
---
+++ resources/views/footer/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="main-footer">
+    <strong>&copy; 2018 SMRPO Informacijski sistem</strong>
+</footer>
