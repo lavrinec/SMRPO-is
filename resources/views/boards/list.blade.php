fb0969ea894fba5b6c6b446a45d179e9a8cd77e6
Marko <marko.laviner>
2018-03-29 12:57:44 +0200

    visual updates

--- resources/views/boards/list.blade.php
+++ resources/views/boards/list.blade.php
@@ -8,11 +8,6 @@
                 Table
                 <small>Seznam tabel</small>
             </h1>
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                <li><a href="#">Tables</a></li>
-                <li class="active">Data tables</li>
-            </ol>
         </section>
 
         <!-- Main content -->
