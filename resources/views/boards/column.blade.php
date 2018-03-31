8f6a54138ee5741e036c407cfed7ba31e0d9d612
Žiga Černigoj <zc6857@student.uni-lj.si>
2018-03-31 20:00:17 +0200

    added unique ids for input fields in board editor

--- resources/views/boards/column.blade.php
+++ resources/views/boards/column.blade.php
@@ -7,11 +7,11 @@
             <h5 class="box-title">
 
                 <div class="form-group">
-                    <label for="column_name" class="col-sm-2 control-label">Ime</label>
+                    <label for="{{ $column_id }}_column_name" class="col-sm-2 control-label">Ime</label>
 
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="column_name" name="column_name"
-                               placeholder="Ime" pattern=".{1,255}" required
+                        <input type="text" class="form-control" id="{{ $column_id }}_column_name"
+                               name="{{ $column_id }}_column_name" placeholder="Ime" pattern=".{1,255}" required
                                title="Ime naj bo dolgo med 8 in 255 znakov">
                     </div>
                 </div>
@@ -23,53 +23,50 @@
             <div class="content">
 
                 <div class="form-group">
-                    <label for="description" class="col-sm-3 control-label">Opis</label>
+                    <label for="{{ $column_id }}_description" class="col-sm-3 control-label">Opis</label>
 
                     <div class="col-sm-9">
-                        <input type="text" class="form-control" id="description" name="description"
-                               placeholder="Opis" pattern=".{1,255}" required
+                        <input type="text" class="form-control" id="{{ $column_id }}_description"
+                               name="{{ $column_id }}_description" placeholder="Opis" pattern=".{1,255}" required
                                title="Opis naj bo dolg med 1 in 255 znakov">
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <label for="wip" class="col-sm-3 control-label">WIP</label>
+                    <label for="{{ $column_id }}_wip" class="col-sm-3 control-label">WIP</label>
 
                     <div class="col-sm-9">
-                        <input type="number" class="form-control" id="wip" name="wip">
+                        <input type="number" class="form-control" id="{{ $column_id }}_wip" name="{{ $column_id }}_wip"
+                               required title="Vpišite omejitev WIP">
                     </div>
                 </div>
 
 
                 <div class="form-group">
-                    <label for="types" class="col-sm-3 control-label">Vloge</label>
+                    <label for="{{ $column_id }}_types[]" class="col-sm-3 control-label">Vloge</label>
                     <div class="col-sm-9">
 
-                        <label for="start_border" class="control-sidebar-subheading">
-                            <input id="start_border" name="types[]"
-                                   value="start_border" type="checkbox"
-                                   class="pull-left">
+                        <label for="{{ $column_id }}_start_border" class="control-sidebar-subheading">
+                            <input id="{{ $column_id }}_start_border" name="{{ $column_id }}_types[]"
+                                   value="start_border" type="checkbox" class="pull-left">
                             Začetni robni
                         </label>
 
-                        <label for="end_border" class="control-sidebar-subheading">
-                            <input id="end_border" name="types[]"
-                                   value="end_border" type="checkbox"
-                                   class="pull-left">
+                        <label for="{{ $column_id }}_end_border" class="control-sidebar-subheading">
+                            <input id="{{ $column_id }}_end_border" name="{{ $column_id }}_types[]"
+                                   value="end_border" type="checkbox" class="pull-left">
                             Končni robni
                         </label>
 
-                        <label for="high_priority" class="control-sidebar-subheading">
-                            <input id="high_priority" name="types[]"
-                                   value="high_priority" type="checkbox"
-                                   class="pull-left">
+                        <label for="{{ $column_id }}_high_priority" class="control-sidebar-subheading">
+                            <input id="{{ $column_id }}_high_priority" name="{{ $column_id }}_types[]"
+                                   value="high_priority" type="checkbox" class="pull-left">
                             Za nujne kartice
                         </label>
 
-                        <label for="acceptance_testing" class="control-sidebar-subheading">
-                            <input id="acceptance_testing" name="types[]"
-                                   value="acceptance_testing" type="checkbox"
-                                   class="pull-left">
+                        <label for="{{ $column_id }}_acceptance_testing" class="control-sidebar-subheading">
+                            <input id="{{ $column_id }}_acceptance_testing" name="{{ $column_id }}_types[]"
+                                   value="acceptance_testing" type="checkbox" class="pull-left">
                             Sprejemno testiranje
                         </label>
 
@@ -89,7 +86,8 @@
                         <i class="fa fa-trash"></i>
                     </button>
 
-                    <button id="{{ $column_id }}_addFirstSubcolumn" type="button" class="btn btn-default col-sm-3" onclick="addSubColumnTo({{ $column_id }})">
+                    <button id="{{ $column_id }}_addFirstSubcolumn" type="button" class="btn btn-default col-sm-3"
+                            onclick="addSubColumnTo({{ $column_id }})">
                         <i class="fa fa-arrow-down"></i>
                     </button>
 
@@ -101,7 +99,8 @@
                 </div>
             </div>
 
-            <div class="canvas subcanvas row" id="{{ $column_id }}_subcanvas" style="margin-left: 0px; margin-right: 0px">
+            <div class="canvas subcanvas row" id="{{ $column_id }}_subcanvas"
+                 style="margin-left: 0px; margin-right: 0px">
 
             </div>
         </div>
