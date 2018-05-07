c001fe30dc8cc475f8e4ac2da51228b4693d625b
Žiga Černigoj <zc6857@student.uni-lj.si>
2018-05-07 16:54:00 +0200

    silver bullet cards now with asterisk (star) for more visibility

--- resources/views/boards/card.blade.php
+++ resources/views/boards/card.blade.php
@@ -1,8 +1,15 @@
-<div class="box grabbable" style="background-color: {{ $card->color }}; " ondblclick="openCard({{ $card->id }})" data-card-id="{{ $card->id }}">
+<div class="box grabbable" style="background-color: {{ $card->color }}; " ondblclick="openCard({{ $card->id }})"
+     data-card-id="{{ $card->id }}">
     <div class="box-header">
         <small>{{ $card->order }}</small>
         <h5 class="box-title">
             {{ $card->card_name }}
+
+
+                @if(isset($card->is_silver_bullet) && ($card->is_silver_bullet == 1 || $card->is_silver_bullet == "true"))
+                    <i class="fa fa-asterisk" title="nujna kartica"></i>
+                @endif
+           
         </h5>
         <br>
         <small>pooblaščeni:
@@ -18,7 +25,7 @@
     </div>
 
     {{--<div class="box-body">--}}
-        {{----}}
+    {{----}}
     {{--</div>--}}
 
 </div>
